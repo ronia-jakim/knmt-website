45807803ba1486f81f894e8a29111c124b672053
maruda <weronikajakimowicz2@gmail.com>
2024-10-26 21:39:14 +0200

    build-update

--- build.sh
+++ build.sh
@@ -9,9 +9,10 @@ mkdir -p build/stylesheets
 cp -r assets build 
 cp -r content build/content
 
-sass ./src/stylesheets/colors_fonts.scss ./build/stylesheets/colors_fonts.css
+# sass ./src/stylesheets/colors_fonts.scss ./build/stylesheets/colors_fonts.css
 sass ./src/stylesheets/basic-style.scss ./build/stylesheets/basic-style.css
 sass ./src/stylesheets/news.scss ./build/stylesheets/news.css
+sass ./src/stylesheets/photos.scss ./build/stylesheets/photos.css
 
 
 if [[ " $* " == *" --github-pages "* ]]; then
