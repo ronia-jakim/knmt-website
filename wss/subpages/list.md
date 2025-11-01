7d8ae82e7865ac53de0dac3f1939739f4a999151
Krzysztof Szymański <kszymanski926@gmail.com>
2025-11-01 18:47:57 +0100

    Registration page for WSS 2025

--- wss/subpages/list.md
+++ wss/subpages/list.md
@@ -1,5 +1,7 @@
 ---
 subpages:
     - { name: 'Informacje', url: '/wss/' }
+    - { name: 'Rejestracja', url: '/wss/registration' }
+    - { name: 'Program konferencji', url: '/wss/programme' }
     - { name: 'KNMT UWr', url: '/', special: true }
 ---
---
+++ wss/subpages/registration/src/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import frontmatter
+import os
+import traceback
+
+from base.src.basic_functions import getEnvironment, getSubpages
+
+INFORMATION_FILE_PATH = 'content/wss/registration.md'
+
+def main():
+    env = getEnvironment()
+
+    template = env.get_template('subpages/registration/index.html')
+    html_cnt = ''
+    subpages = getSubpages()
+    try:
+        with open(INFORMATION_FILE_PATH) as f:
+            info = frontmatter.load(f)
+            html_cnt = template.render(p = info, uczestnicy=uczestnicy, referaty=referaty)
+    except Exception:
+        print('could not read info')
+        traceback.print_exc()
+
+    try:
+        os.makedirs('build/wss/registration')
+    except Exception as inst:
+        print(f'could not create a directory for info page: {inst}')
+
+    try:
+        with open("build/wss/registration/index.html", mode="w", encoding="utf-8") as m:
+            m.write(html_cnt)
+    except Exception as inst:
+        print(f'could not write to info/index.html: {inst}')
+
+if __name__ == '__main__':
+    main()
